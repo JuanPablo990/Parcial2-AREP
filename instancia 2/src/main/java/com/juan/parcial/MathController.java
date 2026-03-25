f4bb2681887dfa631e70e42b029be8566ba1b91a
juanpablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2026-03-25 11:22:50 -0500

    feat: haciendo el match controller para instance2

--- instancia 2/src/main/java/com/juan/parcial/MathController.java
+++ instancia 2/src/main/java/com/juan/parcial/MathController.java
@@ -2,7 +2,7 @@ package com.juan.parcial;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
 
--- instancia 2/src/main/java/com/juan/parcial/MathServiceApplication.java
+++ instancia 2/src/main/java/com/juan/parcial/MathServiceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParcialApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ParcialApplication.class, args);
-	}
-
+public class MathServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MathServiceApplication.class, args);
+    }
 }
